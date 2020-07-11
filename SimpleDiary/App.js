55ff24d25715a489015b0a29278ec31a4ee5b6b9
suyeon <merci.suyeon@gmail.com>
2020-07-11 16:06:34 +0900

    diary 작성, 저장

--- SimpleDiary/App.js
+++ SimpleDiary/App.js
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
@@ -8,23 +7,15 @@ import Detail from './pages/Detail';
 import Form from './pages/Form';
 
 
-function HomeScreen() {
-  return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
-      </View>
-  );
-}
-
 const Stack = createStackNavigator();
 
 function App() {
   return (
       <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen name="List" component={List} />
+          <Stack.Screen name="List" component={List} options={ {title: '일기 목록'} }/>
           <Stack.Screen name="Detail" component={Detail} />
-          <Stack.Screen name="Form" component={Form} />
+          <Stack.Screen name="Form" component={Form} options={ {title: '일기 작성'} }/>
 
         </Stack.Navigator>
       </NavigationContainer>
